9d84bc1904efc92259d4591ca7b26b6fff0064d0
amarpal <azs56@acuitysso.com>
2026-05-17 18:19:43 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+
+function App() {
+  return <div className="bg-green-500 text-red-500 text-2xl">Temp</div>;
+}
+
+export default App;
